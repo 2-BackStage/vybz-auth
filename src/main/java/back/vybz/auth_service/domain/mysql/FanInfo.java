3029095bcb0ab157c386c70f123f9fb362d9c128
jijihorang <jiho7213@gmail.com>
2025-05-19 16:13:01 +0900

    feat : FanInfo entity, SocialType ENUM 설계

---
+++ src/main/java/back/vybz/auth_service/domain/mysql/FanInfo.java
@@ -0,0 +1,30 @@
+package back.vybz.auth_service.domain.mysql;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "fan_info")
+@Getter
+@NoArgsConstructor
+public class FanInfo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "user_uuid", nullable = false, unique = true)
+    private String userUuid;
+
+    @Column(name = "user_id", nullable = false, unique = true)
+    private String userId;
+
+    @Column(name = "type", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private SocialType socialType;
+
+    @Column(name = "provider_id", nullable = false)
+    private String providerId;
+
+}
