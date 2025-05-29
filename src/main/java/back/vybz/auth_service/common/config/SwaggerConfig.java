c88365efd0070fdbc4b7bf9a7398c8688f429cf1
jijihorang <jiho7213@gmail.com>
2025-05-29 20:49:25 +0900

    fix: auth-service swagger config 수정

--- src/main/java/back/vybz/auth_service/common/config/SwaggerConfig.java
+++ src/main/java/back/vybz/auth_service/common/config/SwaggerConfig.java
@@ -1,45 +1,42 @@
 package back.vybz.auth_service.common.config;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
-import io.swagger.v3.oas.annotations.servers.Server;
-import org.springdoc.core.models.GroupedOpenApi;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-@OpenAPIDefinition(
-        info = @Info(
-                title = "VYBZ API",
-                version = "v1",
-                description = "VYBZ API Docs"
-        ),
-        servers = {
-                @Server(url = "/", description = "Default Server URL"),
-        },
-        security = {
-                @SecurityRequirement(name = "Authorization")
-        }
-)
-@SecurityScheme(
-        name = "Authorization",
-        type = SecuritySchemeType.HTTP,
-        bearerFormat = "JWT",
-        scheme = "bearer"
-)
 
 @Configuration
 public class SwaggerConfig {
 
+    private static final String BEARER_TOKEN_PREFIX = "Bearer";
+
     @Bean
-    public GroupedOpenApi publicApi() {
-        String[] paths = { "/api/v1/**" };
-        return GroupedOpenApi.builder()
-                .group("public-api")
-                .pathsToMatch(paths)
-                .build();
+    public OpenAPI openAPI() {
+
+        String securityJwtName = "JWT";
+        io.swagger.v3.oas.models.security.SecurityRequirement securityRequirement = new SecurityRequirement().addList(securityJwtName);
+        Components components = new Components()
+                .addSecuritySchemes(securityJwtName, new io.swagger.v3.oas.models.security.SecurityScheme()
+                        .name(securityJwtName)
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme(BEARER_TOKEN_PREFIX)
+                        .bearerFormat(securityJwtName));
+
+        return new OpenAPI()
+                .addSecurityItem(securityRequirement)
+                .components(components)
+                .addServersItem(new Server().url("/auth-service"))
+                .info(apiInfo());
     }
 
+    private io.swagger.v3.oas.models.info.Info apiInfo() {
+        return new Info()
+                .title("VYBZ - AUTH SERVICE 문서")
+                .description("VYBZ AUTH-SERVICE Swagger UI")
+                .version("1.0.0");
+    }
 }
